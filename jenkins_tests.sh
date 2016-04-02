34ff78f69c3084b808d8f01f1210146133ba1d4b
Ian Turgeon <iturgeon@gmail.com>
2016-04-02 19:41:17 -0400

    Splits jenkin test script to multiple scripts
    Changes how widgets are installed and splits that into another script
    Splits test into its own script so you can easily re-run tests

--- jenkins_tests.sh
+++ jenkins_tests.sh
@@ -1,9 +1,7 @@
 # ./channelSay "Building $BUILD_URL"
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SOURCE_DIR="$DIR/app/fuel/packages/materia/vendor/widget/source"
-# USE_SUDO=''
-USE_SUDO='sudo'
 # ghprbActualCommit=maverick/clean-up-the-damn-widget-installer
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+USE_SUDO='sudo' # we run these via sudo on linux, osx is better w/o it
 
 if [ ! -d $DIR/app ]; then
 	git clone git@github.com:ucfcdl/Materia.git app
@@ -35,20 +33,11 @@ $USE_SUDO docker-compose build phpfpm
 
 $USE_SUDO docker-compose run --rm phpfpm composer install
 
-# run the tasks normaly run in phpunit bootstrap that we have to skip
-# because we're going to clone the widgets from jenkins
-$USE_SUDO docker-compose run --rm phpfpm /wait-for-it.sh mysql:3306 -t 20 -- env FUEL_ENV=test php oil r install --install_widgets=false --skip_prompts=true
+# clone the default widgets
+source clone_widgets.sh
 
-# clone and install the widgets from the widget config
-for i in $(grep -oh '\<git.*\.git\>' $DIR/app/fuel/packages/materia/config/widgets.php); do
-	rm -rf $DIR/app/fuel/app/tmp/widget
-	git clone $i --depth=1 $DIR/app/fuel/app/tmp/widget
-	$USE_SUDO docker-compose run --rm phpfpm bash -c 'env FUEL_ENV=test php oil r widget:install fuel/app/tmp/widget/_output/*.wigt'
-	rm -rf $DIR/app/fuel/app/tmp/widget
-done
-
-# run tests!
-$USE_SUDO docker-compose run --rm phpfpm /wait-for-it.sh mysql:3306 -t 20 -- env SKIP_BOOTSTRAP_TASKS=true php oil test
+# install widgets and run tests
+source run_tests.sh
 
 #cd /home/lst/Desktop/materia-docker/app
 #jasmine-node --coffee --verbose --captureExceptions spec/
