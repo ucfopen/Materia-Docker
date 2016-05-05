ca55eb844c3581b64936d60f44f7893d864bfb4e
iturgeon <iturgeon@gmail.com>
2016-05-05 13:41:09 -0400

    update sh commands to new widget installer, add --rm to runs and get rid of deploy keys

--- gulp_watch.sh
+++ gulp_watch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker-compose run node gulp
+docker-compose run --rm node gulp
 
--- install_widget.sh
+++ install_widget.sh
@@ -3,4 +3,4 @@
 # run with: ./install_widget.sh adventure.wigt
 # this does support globs, but you have to quote them so they arent processed in your term
 # run w wildcards: ./install_widget.sh '*.wigt'  OR  ./install_widget.sh 'popup*.wigt'
-docker-compose run --rm phpfpm sh -c 'php oil r widget:install /var/www/html/fuel/app/tmp/widget_packages/'$1
+docker-compose run --rm phpfpm bash -c 'php oil r widget:install fuel/app/tmp/widget_packages/'$1
