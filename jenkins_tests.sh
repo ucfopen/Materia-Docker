1ee4cb79963711eadc59487279c1de2084704958
iturgeon <iturgeon@gmail.com>
2016-06-13 17:55:59 -0400

    adds missing file extension in test commands

--- jenkins_tests.sh
+++ jenkins_tests.sh
@@ -6,12 +6,12 @@ rm -f app/fuel/app/config/development/migrations.php
 rm -f app/fuel/app/config/test/migrations.php
 
 # stop and remove docker containers
-docker-compose -f docker-compose.yml -f docker-compose.admin.ym stop
-docker-compose -f docker-compose.yml -f docker-compose.admin.ym rm -f --all
+docker-compose -f docker-compose.yml -f docker-compose.admin.yml stop
+docker-compose -f docker-compose.yml -f docker-compose.admin.yml rm -f --all
 
-docker-compose -f docker-compose.yml -f docker-compose.admin.ym pull
-docker-compose -f docker-compose.yml -f docker-compose.admin.ym build mysql
-docker-compose -f docker-compose.yml -f docker-compose.admin.ym build phpfpm
+docker-compose -f docker-compose.yml -f docker-compose.admin.yml pull
+docker-compose -f docker-compose.yml -f docker-compose.admin.yml build mysql
+docker-compose -f docker-compose.yml -f docker-compose.admin.yml build phpfpm
 
 docker-compose -f docker-compose.yml -f docker-compose.admin.yml run --rm phpfpm composer install
 
