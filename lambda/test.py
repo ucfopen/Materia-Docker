900d2387a33f93cd16c57b622bf194ca3091bf6d
David Perlaza <superlaza7@gmail.com>
2016-08-03 18:09:32 -0400

    working prototype for resizing thumbnails lambda (not final)

--- lambda/test.py
+++ lambda/test.py
@@ -9,13 +9,15 @@ print handler
 
 app = Flask(__name__)
 
-def sendEvent(filename):
+s3_endpoint = "http://192.168.99.100:10001/default_bucket"
+
+def sendEvent(filename, fd):
 	event = {  
 	   "Records":[  
 	      {  
 	         "s3":{  
 	            "bucket":{  
-	               "name":"mybucket"
+	               "name":"default_bucket"
 	            },
 	            "object":{  
 	               "key": filename
@@ -26,7 +28,9 @@ def sendEvent(filename):
 	   ]
 	}
 
-	handler(event, None)
+	handler(event, None, fd)
+
+
 
 @app.route("/default_bucket", methods=["POST"])
 @crossdomain(origin='*')
@@ -41,17 +45,23 @@ def upload_passthrough():
 	# print file
 
 	print 'firing request'
+	print type(file.stream)
 	response = requests.post(
 		# "http://fakes3:10001/default_bucket", 
-		"http://192.168.99.100:10001/default_bucket",
+		s3_endpoint,
 		files=[('file', (file.filename, file.stream, file.content_type))],
 		data=formData
 	)
 
+	print 'filename', formData['key']
+	if response.status_code == 201 or 200:
+		sendEvent(formData['key'][0], formData)
+	else:
+		print 'some error'
 	return response.text
 
 	# requests.post("http://fakes3:10001/default_bucket")
 
 if __name__ == '__main__':
-	app.run(port=10002, host='0.0.0.0')
-	# app.run(port=10002)
+	# app.run(port=10002, host='0.0.0.0')
+	app.run(port=10002)
