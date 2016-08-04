b7a2554f479d8ce829ffd43e034cc36c864b6461
David Perlaza <superlaza7@gmail.com>
2016-08-04 16:20:59 -0400

    finalizing lambda server

--- lambda/createThumbnail.py
+++ lambda/createThumbnail.py
@@ -1,66 +1,32 @@
-from __future__ import print_function
-import boto3, os, sys, uuid, shutil
+import boto3, os, sys, uuid
 from PIL import Image
 import PIL.Image
-import requests
-import io
 
-s3_client = boto3.client('s3')
-print(s3_client.meta.endpoint_url)
-
-# s3_endpoint = "http://192.168.99.100:10001/default_bucket"
-s3_endpoint = "http://fakes3:10001"
+# set environment, either 'dev' or 'prod'
+ENV = 'dev'
 
 def resize(image_path, resized_path):
 	with Image.open(image_path) as image:
 		image.thumbnail(tuple(x / 3 for x in image.size))
 		image.save(resized_path)
 
-def process_image(key, fd):
-	# get asset from fakes3
-	asset_url = "{}/{}".format(s3_endpoint, key)
-	r = requests.get(asset_url)
-
-	## download to LFS from response
-	pathsplit = key.split('/')
-	filename = pathsplit[-1]
-	basepath = '/'.join(pathsplit[:-1])
-	download_path = '{}.png'.format(filename)
-	if r.status_code < 300:
-		with open(download_path, 'wb') as f:
-			f.write(r.content)
-	else:
-		print('request failed')
-
-	resized_path = 'resize-'+download_path
-	# here's the actual lambda
-	resize(download_path, resized_path)
-
-	new_key = '{}/thumb/{}'.format(basepath, filename)
-	print(new_key)
-	with open(resized_path, 'rb') as f:
-		stream = io.BytesIO(f.read())
-		formData = headers = {
-			'key': new_key
-		}
-		response = requests.post(
-			# "http://fakes3:10001/default_bucket", 
-			s3_endpoint,
-			files=[('file', (filename, stream, 'png'))],
-			data=formData
-		)
-		# res = requests.post(s3_endpoint, data=f)
-		print(response)
-	# s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
-
-
-def handler(event, context, fd):
+def handler(event, context):
 	for record in event['Records']:
 		bucket = record['s3']['bucket']['name']
 		key = record['s3']['object']['key'] 
-		process_image(key, fd)
 
-		# s3_client.download_file(bucket, key, download_path)
-		# resize(download_path, 'resize-'+download_path)
-		# s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
+		if ENV == 'dev':
+			from fakes3_client import fakes3_client
+			s3_client 		= fakes3_client(key)
+			resized_path 	= s3_client.resized_path
+			download_path 	= s3_client.download_path
+			upload_path		= '' # not used, preset
+
+		if ENV == 'prod':
+			s3_client = boto3.client('s3')
+			print s3_client.meta.endpoint_url
+
+		s3_client.download_file(bucket, key, download_path)
+		resize(download_path, resized_path)
+		s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
 		
