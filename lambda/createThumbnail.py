a6848c8ed72c93844f73d2ccc57f902e2c58992a
David Perlaza <superlaza7@gmail.com>
2016-08-04 12:48:35 -0400

    working thumbnails

--- lambda/createThumbnail.py
+++ lambda/createThumbnail.py
@@ -8,43 +8,59 @@ import io
 s3_client = boto3.client('s3')
 print(s3_client.meta.endpoint_url)
 
-s3_endpoint = "http://192.168.99.100:10001/default_bucket"
+# s3_endpoint = "http://192.168.99.100:10001/default_bucket"
+s3_endpoint = "http://fakes3:10001"
 
-def resize_image(image_path, resized_path):
+def resize(image_path, resized_path):
 	with Image.open(image_path) as image:
-		image.thumbnail(tuple(x / 2 for x in image.size))
+		image.thumbnail(tuple(x / 3 for x in image.size))
 		image.save(resized_path)
-	 
+
+def process_image(key, fd):
+	# get asset from fakes3
+	asset_url = "{}/{}".format(s3_endpoint, key)
+	r = requests.get(asset_url)
+
+	## download to LFS from response
+	pathsplit = key.split('/')
+	filename = pathsplit[-1]
+	basepath = '/'.join(pathsplit[:-1])
+	download_path = '{}.png'.format(filename)
+	if r.status_code < 300:
+		with open(download_path, 'wb') as f:
+			f.write(r.content)
+	else:
+		print('request failed')
+
+	resized_path = 'resize-'+download_path
+	# here's the actual lambda
+	resize(download_path, resized_path)
+
+	new_key = '{}/thumb/{}'.format(basepath, filename)
+	print(new_key)
+	with open(resized_path, 'rb') as f:
+		stream = io.BytesIO(f.read())
+		formData = headers = {
+			'key': new_key
+		}
+		response = requests.post(
+			# "http://fakes3:10001/default_bucket", 
+			s3_endpoint,
+			files=[('file', (filename, stream, 'png'))],
+			data=formData
+		)
+		# res = requests.post(s3_endpoint, data=f)
+		print(response)
+	# s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
+
+
 def handler(event, context, fd):
 	for record in event['Records']:
 		bucket = record['s3']['bucket']['name']
 		key = record['s3']['object']['key'] 
-		# download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
-		download_path = key
-		# upload_path = '/tmp/resized-{}'.format(key)
-		upload_path = 'thumb-{}'.format(key)
+		process_image(key, fd)
 
 		# s3_client.download_file(bucket, key, download_path)
-		r = requests.get("{}/{}".format(s3_endpoint, key))
-		download_path = '{}.png'.format(key.split('/')[-1])
-		if r.status_code == 200:
-			with open(download_path, 'wb') as f:
-				f.write(r.content)
-		else:
-			print('request failed')
-
-		resize_image(download_path, 'resize-'+download_path)
-
-		with open('resize-'+download_path, 'rb') as f:
-			stream = io.BytesIO(f.read())
-			fd['key'] = 'somenewkey'
-			response = requests.post(
-				# "http://fakes3:10001/default_bucket", 
-				s3_endpoint,
-				files=[('file', ('bullshit', stream, 'png'))],
-				data=fd
-			)
-			# res = requests.post(s3_endpoint, data=f)
-			print(response)
+		# resize(download_path, 'resize-'+download_path)
 		# s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
 		
--- lambda/test.py
+++ lambda/test.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from util import crossdomain
 import requests
 
+
 # get Lambda handler
 from createThumbnail import handler
 
@@ -9,7 +10,8 @@ print handler
 
 app = Flask(__name__)
 
-s3_endpoint = "http://192.168.99.100:10001/default_bucket"
+# s3_endpoint = "http://192.168.99.100:10001/default_bucket"
+s3_endpoint = "http://fakes3:10001"
 
 def sendEvent(filename, fd):
 	event = {  
@@ -17,7 +19,7 @@ def sendEvent(filename, fd):
 	      {  
 	         "s3":{  
 	            "bucket":{  
-	               "name":"default_bucket"
+	               "name":"fakes3"
 	            },
 	            "object":{  
 	               "key": filename
@@ -32,17 +34,18 @@ def sendEvent(filename, fd):
 
 
 
-@app.route("/default_bucket", methods=["POST"])
+@app.route("/fakes3", methods=["POST"])
 @crossdomain(origin='*')
 def upload_passthrough():
 	headers = dict(request.headers)
 
 	formData = dict(request.form)
 
-	file = request.files['file']
-	# del params['file']
+	print formData
 
-	# print file
+	file = request.files['file']
+	
+	print file.mimetype, file.mimetype_params, file.content_type
 
 	print 'firing request'
 	print type(file.stream)
@@ -53,15 +56,22 @@ def upload_passthrough():
 		data=formData
 	)
 
-	print 'filename', formData['key']
-	if response.status_code == 201 or 200:
-		sendEvent(formData['key'][0], formData)
+	print 'filename', formData['key'], file.filename, file.content_type
+	if response.status_code == 201 or response.status_code == 200:
+		try:
+			sendEvent(formData['key'][0], formData)
+
+		except Exception as e:
+			print 'failed to resize image'
+
+		return response.text
 	else:
-		print 'some error'
-	return response.text
+		msg = 'failed to communicate with fakes3 server'
+		print msg
+		return msg
 
 	# requests.post("http://fakes3:10001/default_bucket")
 
 if __name__ == '__main__':
-	# app.run(port=10002, host='0.0.0.0')
-	app.run(port=10002)
+	app.run(port=10002, host='0.0.0.0')
+	# app.run(port=10002)
