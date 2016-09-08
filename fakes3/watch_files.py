fe4bd9d9ed7fa0d4907f652f961bff3d03d17e23
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2016-09-08 15:27:12 -0400

    Commented file and removed extraneous imports

--- fakes3/watch_files.py
+++ fakes3/watch_files.py
@@ -1,12 +1,12 @@
-import sys, os, re
+import os, re
 import time
-import logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from createThumbnail import handler
 
 is_asset_id = re.compile('^\w{5}\.\w*?$')
 
+# Creates an object that mocks the event object sent to lambda from Fakes3
 get_event = lambda image_key: {
    "Records":[
       {
@@ -22,27 +22,30 @@ get_event = lambda image_key: {
    ]
 }
 
-class MyHandler(FileSystemEventHandler):
+class FileHandler(FileSystemEventHandler):
     def on_created(self, event):
-        # filters all filenames that are alphanumeric and then takes the file
-        #   with a length of 5
         uploadedFile = os.path.split(event.src_path)[1]
+
+        # Creates a relative path to the created file within the filesystem
         image_key = '/'.join(event.src_path.split('/')[-3:])
+
+        # Makes sure a thumbnail of a thumbnail is not being created.
         is_thumb = image_key.find('thumb') != -1
         if is_asset_id.match(uploadedFile) and not is_thumb:
+            print "IMAGE KEY: ", image_key
             fakes3_event = get_event(image_key)
             handler(fakes3_event, None)
 
 if __name__ == "__main__":
-    path = '../s3mnt/fakes3_root/fakes3/uploads/'
-    event_handler = MyHandler()
+    path_being_watched = '../s3mnt/fakes3_root/fakes3/uploads/'
+    event_handler = FileHandler()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, path_being_watched, recursive=True)
     observer.start()
     try:
         while True:
+            #This keeps Materia from running extremely slow
             time.sleep(1)
-            print "Watching " + path
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
