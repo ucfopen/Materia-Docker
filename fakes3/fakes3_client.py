476b5813b82108f238fd0fe186d991bd9b44e9fb
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2017-03-10 10:47:09 -0500

    Rewrote local thumbanil-generator process to act the exact same as the handler placed on amazon's lambda service

--- fakes3/fakes3_client.py
+++ fakes3/fakes3_client.py
@@ -1,49 +1,41 @@
 import io, requests
+import cStringIO
 
 '''
 Receives upload requests from local lambda. This client was built to mimic
 how the lambda handler will talk to the real s4 server.
 '''
 class fakes3_client:
-	def __init__(self, key):
-		self.endpoint_url = "http://127.0.0.1:10001/fakes3"
-		self.key = key
-		pathsplit = key.split('/')
-		self.basepath = '/'.join(pathsplit[-2:])
-		self.filename = pathsplit[-1]
-
-		self.download_path = '{}'.format(self.filename)
-		self.resized_path = 'resize-'+self.download_path
+	def __init__(self):
+		self.endpoint_url = "http://127.0.0.1:10001"
 
 	# get asset from fakes3
-	def download_file(self, bucket, key, download_path):
-		asset_url = "{}/{}".format(self.endpoint_url, self.key)
+	def get_object(self, Bucket, Key):
+		asset_url = "{}/{}".format(self.endpoint_url, Key)
+		print asset_url
 		response = requests.get(asset_url)
 
 		## download to local FS from response
 		if response.status_code < 300:
-			with open(self.download_path, 'wb') as f:
-				f.write(response.content)
-		else:
-			print('download request failed')
-
-
-	def upload_file(self, upload_path, filename, key):
-		new_key = '/thumbnails/{}'.format(self.basepath)
-		with open(self.resized_path, 'rb') as f:
-			stream = io.BytesIO(f.read())
-			formData = headers = {
-				'key': new_key
+			image = cStringIO.StringIO(response.content)
+			return {
+				"Body": image
 			}
 
-			filetype = key.split('.')[1]
-			response = requests.post(
-				# "http://fakes3:10001/default_bucket",
-				self.endpoint_url,
-				files=[('file', (new_key, stream, filetype))],
-				data=formData
-			)
-			if response.status_code < 300:
-				print('upload success')
-			else:
-				print('upload request failed')
+	def upload_fileobj(self, Fileobj, Bucket, Key):
+		formData = headers = {
+			'key': Key
+		}
+
+		filetype = Key.split('.')[1]
+
+		response = requests.post(
+			"{}/{}/".format(self.endpoint_url, Key),
+			files=[('file', (Key, Fileobj, filetype))],
+			data=formData
+		)
+
+		if response.status_code < 300:
+			print('upload success')
+		else:
+			print('upload failed')
