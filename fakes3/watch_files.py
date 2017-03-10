07990f5dff02460a8768bec72d55877291af31f3
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2017-03-10 10:47:09 -0500

    Rewrote local thumbanil-generator process to act the exact same as the handler placed on amazon's lambda service

--- fakes3/watch_files.py
+++ fakes3/watch_files.py
@@ -2,9 +2,9 @@ import os, re
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from createThumbnail import handler
+from handler import upload_thumbnail
 
-is_asset_id = re.compile('^\w{5}\.\w*?$')
+is_asset_id = re.compile('\w\-(\w{5})\.')
 
 # Creates an object that mocks the event object sent to lambda from Fakes3
 get_event = lambda image_key: {
@@ -25,19 +25,16 @@ get_event = lambda image_key: {
 class FileHandler(FileSystemEventHandler):
     def on_created(self, event):
         uploadedFile = os.path.split(event.src_path)[1]
-
         # Creates a relative path to the created file within the filesystem
         image_key = '/'.join(event.src_path.split('/')[-3:])
-        print "IMAGE KEY: " + image_key;
-
         # Makes sure a thumbnail of a thumbnail is not being created.
-        is_thumb = image_key.find('thumb') != -1
+        is_thumb = image_key.find('thumbnails') != -1
         if is_asset_id.match(uploadedFile) and not is_thumb:
             fakes3_event = get_event(image_key)
-            handler(fakes3_event, None)
+            upload_thumbnail(fakes3_event, None)
 
 if __name__ == "__main__":
-    path_being_watched = '../s3mnt/fakes3_root/fakes3/dev_uploads/'
+    path_being_watched = '../s3mnt/fakes3_root/fakes3/uploads/'
     event_handler = FileHandler()
     observer = Observer()
     observer.schedule(event_handler, path_being_watched, recursive=True)
