e58bd59d288d7fffc1f05b7a85c6713188bc8150
Ian Turgeon <iturgeon@gmail.com>
2017-07-07 14:10:40 -0400

    change fakes3 box to use alpine

--- firstrun.sh
+++ firstrun.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+NODE_DC_COMMAND="docker-compose -f docker-compose.yml -f docker-compose.admin.yml"
 DOCKER_IP="localhost"
 # Use docker or set up the docker-machine environment
 if hash docker 2>/dev/null; then
@@ -38,21 +39,22 @@ if [ -f  app/fuel/app/config/development/migrations.php ]; then
 	rm -f app/fuel/app/config/development/migrations.php
 fi
 
-docker-compose run --rm phpfpm bash -c '/wait-for-it.sh mysql:3306 -t 20 -- php oil r install --skip_install_widgets --skip_prompts=true --skip_configuration_wizard'
+docker-compose run --rm phpfpm bash -c '/wait-for-it.sh mysql:3306 -t 20 -- php oil r install --skip_prompts=true --skip_configuration_wizard'
 
 # source clone_widgets.sh
 
 docker-compose run --rm phpfpm bash -c 'php oil r widget:install fuel/app/tmp/widget_packages/*.wigt'
 
 # # install all the needed npm stuff
-$USE_SUDO docker-compose -f docker-compose.yml -f docker-compose.admin.yml run --rm node yarn --pure-lockfile --force
+$USE_SUDO $NODE_DC_COMMAND run --rm node yarn install --no-progress --pure-lockfile --force
 
 # # compile js and css
-$USE_SUDO docker-compose -f docker-compose.yml -f docker-compose.admin.yml run --rm node gulp js css hash
+$USE_SUDO $NODE_DC_COMMAND run --rm node gulp js css hash
 
 # run that beast
 # Use docker or set up the docker-machine environment
-echo Materia will be hosted on $DOCKER_IP
-echo Run gulp: docker-compose -f docker-compose.yml -f docker-compose.admin.yml run --rm node gulp js css hash
-echo Run an oil comand: docker-compose run --rm phpfpm php oil r
-echo Run the web app: docker-compose up
+echo -e "Materia will be hosted on \033[32m$DOCKER_IP\033[0m"
+echo -e "\033[1mRun gulp:\033[0m $NODE_DC_COMMAND run --rm node gulp js css hash"
+echo -e "\033[1mRun an oil comand:\033[0m docker-compose run --rm phpfpm php oil r"
+echo -e "\033[1mRun the web app:\033[0m docker-compose up"
+
