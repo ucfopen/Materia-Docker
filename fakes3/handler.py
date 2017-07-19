bd151637af04ac936d18767ca0fa8575520afa0c
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2017-07-19 12:54:45 -0400

    Removed unnecessary items from fakes3 box, add environment variable to the fakes3 box to determine if the original image should be copied over to second bucket, changed handler to also move original image to bucket that thumbnails are going to

--- fakes3/handler.py
+++ fakes3/handler.py
@@ -6,52 +6,72 @@ import StringIO
 
 s3_client = fakes3_client()
 
+
+def _process_image(image, extension, resize=None):
+    image = Image.open(image)
+    image_data = StringIO.StringIO()
+
+    if resize:
+        image.thumbnail(resize)
+
+    image.save(image_data, extension)
+    image.close()
+
+    # image.save is not seeking to beginning of buffer upon completion this is a manual reset
+    image_data.seek(0)
+
+    return image_data
+
+
 def upload_thumbnail(event, context):
-	'''
-	RESPONDS TO FakeS3 EVENTS FROM BUCKET PUT
-	Retrieves file, checks format, resizes, uploads resized to target
-	'''
-	for record in event['Records']:
-		source_bucket = record['s3']['bucket']['name']
-		source_key = record['s3']['object']['key']
+    '''
+    RESPONDS TO FakeS3 EVENTS FROM BUCKET PUT
+    Retrieves file, checks format, resizes, uploads resized to target
+    '''
+    for record in event['Records']:
+        source_bucket = record['s3']['bucket']['name']
+        source_key = record['s3']['object']['key']
 
-		# build up the output variables
-		output_bucket = os.environ['OUTPUT_BUCKET']
-		max_size = os.environ['OUTPUT_MAX_HEIGHT'] + "x" + os.environ['OUTPUT_MAX_WIDTH']
-		output_size = int(os.environ['OUTPUT_MAX_HEIGHT']), \
-			int(os.environ['OUTPUT_MAX_WIDTH'])
-		output_key = os.environ['OUTPUT_BASE_KEY']
+        # build up the output variables
+        output_bucket = os.environ['OUTPUT_BUCKET']
+        max_size = os.environ['OUTPUT_MAX_HEIGHT'] + \
+            "x" + os.environ['OUTPUT_MAX_WIDTH']
+        output_size = int(os.environ['OUTPUT_MAX_HEIGHT']), \
+            int(os.environ['OUTPUT_MAX_WIDTH'])
+        output_key = os.environ['OUTPUT_BASE_KEY']
 
-		filename = os.path.basename(source_key)
-		base, extension = os.path.splitext(filename)
-		extension = extension[1:].upper()
+        filename = os.path.basename(source_key)
+        base, extension = os.path.splitext(filename)
+        extension = extension[1:].upper()
 
-		if extension not in ['JPEG', 'JPG', 'PNG', 'GIF']:
-			print "Uploaded file extension is not supported: %s" % extension
-			return None
+        if extension not in ['JPEG', 'JPG', 'PNG', 'GIF']:
+            print "Uploaded file extension is not supported: %s" % extension
+            return None
 
-		if extension == 'JPG':
-			extension = 'JPEG'
+        if extension == 'JPG':
+            extension = 'JPEG'
 
-		# Download the image from s3 into memory
-		uploaded_object = s3_client.get_object(Bucket=source_bucket,
-											   Key=source_key)
+        # Download the image from s3 into memory
+        uploaded_object = s3_client.get_object(Bucket=source_bucket,
+                                               Key=source_key)
 
-		# body is a file like object
-		uploaded_object_body = uploaded_object['Body']
+        # body is a file like object
+        uploaded_object_body = uploaded_object['Body']
 
-		image = Image.open(uploaded_object_body)
+        if(os.environ['OUTPUT_ORIGINAL']):
+            original_image_data = _process_image(
+                uploaded_object_body, extension)
 
-		image.thumbnail(output_size)
-		resized_image_data = StringIO.StringIO()
+            original_output_key = output_key + "/" + base + "." + extension.lower()
 
-		image.save(resized_image_data, extension)
-		image.close()
+            s3_client.upload_fileobj(Fileobj=original_image_data,
+                                     Bucket=output_bucket, Key=original_output_key)
 
-		# image.save is not seeking to beginning of buffer upon completion
-        # this is a manual reset
-		resized_image_data.seek(0)
+        resized_image_data = _process_image(
+            uploaded_object_body, extension, output_size)
 
-		output_key = output_key+"/"+base+"-"+max_size+"."+extension.lower()
-		s3_client.upload_fileobj(Fileobj=resized_image_data,
-                                 Bucket=output_bucket, Key=output_key)
+        resized_output_key = output_key + "/" + base + \
+            "-" + max_size + "." + extension.lower()
+
+        s3_client.upload_fileobj(Fileobj=resized_image_data,
+                                 Bucket=output_bucket, Key=resized_output_key)
