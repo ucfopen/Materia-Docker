01522b8d403800f7c09d51e460a0893765266b95
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2017-08-21 11:27:32 -0400

    Changed the thumbnail handler to work in development and on lambda

--- fakes3/watch_files.py
+++ fakes3/watch_files.py
@@ -1,4 +1,5 @@
-import os, re
+import os
+import re
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -8,20 +9,23 @@ is_asset_id = re.compile('\w\-(\w{5})\.')
 is_resized = re.compile('\w\-(\w{5})-\w+x\w+.')
 
 # Creates an object that mocks the event object sent to lambda from Fakes3
-get_event = lambda image_key: {
-   "Records":[
-      {
-         "s3":{
-            "bucket":{
-               "name":"fakes3"
-            },
-            "object":{
-               "key": image_key
+
+
+def get_event(image_key): return {
+    "Records": [
+        {
+            "s3": {
+                "bucket": {
+                    "name": os.environ['INPUT_BUCKET']
+                },
+                "object":{
+                    "key": image_key
+                }
             }
-         }
-      }
-   ]
+        }
+    ]
 }
+
 
 class FileHandler(FileSystemEventHandler):
     def on_created(self, event):
@@ -30,7 +34,7 @@ class FileHandler(FileSystemEventHandler):
         sizeArray = sizes.split(',')
 
         # Creates a relative path to the created file within the filesystem
-        image_key = '/'.join(event.src_path.split('/')[-3:])
+        image_key = '/'.join(event.src_path.split('/')[-2:])
 
         if is_asset_id.match(uploadedFile) and not is_resized.match(uploadedFile):
             for size in sizeArray:
@@ -49,6 +53,7 @@ class FileHandler(FileSystemEventHandler):
                 # the handler never has to change because the environment it runs in is changing
                 upload_thumbnail(fakes3_event, None)
 
+
 if __name__ == "__main__":
     path_being_watched = '../s3mnt/fakes3_root/'
     event_handler = FileHandler()
@@ -57,7 +62,7 @@ if __name__ == "__main__":
     observer.start()
     try:
         while True:
-            #This keeps Materia from running extremely slow
+            # This keeps Materia from running extremely slow
             time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
